07cad43f89ab5d139877097687b3087cafa41169
SebastianPintoSAPT <sebastianalbertopintotorres@gmail.com>
2025-09-25 15:29:08 -0500

    operaciones hechas

--- main.java
+++ main.java
@@ -1,12 +1,19 @@
-#archivo principal
+int suma = 0;
+int resta = 0;
+int multiplicacion = 0;
+float division = 0;
 
-import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        jfjfjfjfkkjkkkkkkkkkkkkkkkkkkkk
+        Calculadora calc = new Calculadora();
+        suma = calc.sumar(5, 3);
+        resta = calc.restar(5, 3);
+        multiplicacion = calc.multiplicar(5, 3);
+        division = calc.dividir(5, 3);
 
-
-
-
-    Kimi no trabaja
-    
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        System.out.println("Division: " + division);
+    }
+}
