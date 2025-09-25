d2fe206190971e26778493286e371c17c052ea33
SebastianPintoSAPT <sebastianalbertopintotorres@gmail.com>
2025-09-25 15:27:52 -0500

    estas son pruebas

--- main.java
+++ main.java
@@ -1,1 +1,12 @@
 #archivo principal
+
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        jfjfjfjfkkjkkkkkkkkkkkkkkkkkkkk
+
+
+
+
+    Kimi no trabaja
+    
