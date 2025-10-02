e23a80ab7e1c44c4e6c45f0a952eceb1d4c28cd1
miguelangeltorresmartinez07 <miguelangeltorresmartinez07@gmail.com>
2025-10-02 12:27:28 -0500

    Hubo un error, en el que se pusieron clases para cada funcion de la calculadora, entonces se dejaron como metodos

--- Calculadora.java
+++ Calculadora.java
@@ -1,17 +1,13 @@
 public class Calculadora {
-    public class suma {
-        public int sumar(int a, int b) {
-            return a + b;
-        }
+    public int sumar(int a, int b) {
+        return a + b;
     }
-    public class resta {
-        public int restar(int a, int b) {
-            return a - b;
-        }
+
+    public int restar(int a, int b) {
+        return a - b;
     }
-    public class multiplicacion {
-        public int multiplicar(int a, int b) {
-            return a * b;
-        }
-    }                       
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
 }
