eb14820cb896ae2dbee78e35fd5a932cbc2c5a0e
miguelangeltorresmartinez07 <miguelangeltorresmartinez07@gmail.com>
2025-10-02 12:18:30 -0500

    3 operaciones basicas

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,17 @@
 public class Calculadora {
-    
+    public class suma {
+        public int sumar(int a, int b) {
+            return a + b;
+        }
+    }
+    public class resta {
+        public int restar(int a, int b) {
+            return a - b;
+        }
+    }
+    public class multiplicacion {
+        public int multiplicar(int a, int b) {
+            return a * b;
+        }
+    }                       
 }
